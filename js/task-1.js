8268d6c278b011c0bb14d639608b18125989a033
AlexandraMarkova <a.markova@ukr.net>
2020-12-18 17:42:32 +0200

    выполняет первое задание ДЗ

---
+++ js/task-1.js
@@ -0,0 +1,34 @@
+const itemsRef = [...document.querySelectorAll("li.item")];
+console.log(`В списке ${itemsRef.length} категории.`)
+
+
+const categories = itemsRef.map(item => { 
+    const textRef = item.querySelector("h2");
+    const elementsRef = item.querySelectorAll("ul li");
+
+    console.log(`Категория: ${textRef.textContent}`);
+    console.log(`Количество элементов: ${elementsRef.length}`); 
+})
+
+
+
+
+// const textRef = document.querySelectorAll("h2");
+// console.log(textRef)
+// const text = textRef.forEach(function (el) {
+//     console.log(`Категория: ${el.textContent}`)
+// });
+
+// const elementsRef = document.querySelectorAll("li>ul");
+// console.log(elementsRef)
+// const element = elementsRef.forEach(el => console.dir(`Количество элементов: ${el.children.length}`));
+
+
+
+
+
+
+
+
+
+
